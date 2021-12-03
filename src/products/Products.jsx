a0bc0da783f2815543627f02da419e0c676195b9
Ramiro Sanchez Lopez <sanchezlramiro@gmail.com>
2021-12-02 23:46:47 -0300

    Cambios en productos

--- src/products/Products.jsx
+++ src/products/Products.jsx
@@ -1,6 +1,10 @@
 import img1 from "./img/img1.png"
 import img2 from "./img/img2.jpg"
 import img3 from "./img/img3.jpg"
+import img4 from "./img/img4.png"
+import img5 from "./img/img5.png"
+import img6 from "./img/img6.jpg"
+
 
 const Products = [
     {
@@ -14,26 +18,26 @@ const Products = [
     {
         id: 2,
         category: "computadoras",
-        title:"Notebook ASUS X509JA",
-        description:"El ASUS X509 es un portátil que ofrece un rendimiento potente y una experiencia visual inmersiva",
-        price: 105000,
-        img: img2
+        title:"MacBook Air M1",
+        description:"El Apple MacBook Air de 13 pulgadas incluye el primer chip de Apple diseñado específicamente para Mac",
+        price: 250000,
+        img: img5
     },
     {
         id: 3,
-        category: "tablets",
-        title:"Ipad 10 128 GB",
-        description:"El nuevo iPad se transforma en lo que quieras",
-        price: 85000,
-        img: img3
+        category: "celulares",
+        title:"Iphone 13 256gb",
+        description:"iPhone 13. El sistema de dos cámaras más avanzado en un iPhone. El superrápido chip A15 Bionic.",
+        price: 215000,
+        img: img4
     },
     {
         id: 4,
         category: "celulares",
-        title:"Galaxy S21+",
-        description:"El Samsung Galaxy S21+ es la variante con pantalla grande de la serie Galaxy S21",
-        price: 150000,
-        img: img1
+        title:"Pixel 6 128 GB",
+        description:"Pixel 6. Está completamente reinventado, por dentro y por fuera",
+        price: 20000,
+        img: img6
     },
     {
         id: 5,
@@ -54,10 +58,10 @@ const Products = [
     {
         id: 7,
         category: "celulares",
-        title:"Galaxy S21+",
-        description:"El Samsung Galaxy S21+ es la variante con pantalla grande de la serie Galaxy S21",
-        price: 150000,
-        img: img1
+        title:"Iphone 13 256gb",
+        description:"iPhone 13. El sistema de dos cámaras más avanzado en un iPhone. El superrápido chip A15 Bionic.",
+        price: 215000,
+        img: img4
     },
     {
         id: 8,
@@ -69,11 +73,11 @@ const Products = [
     },
     {
         id: 9,
-        category: "tablets",
-        title:"Ipad 10 128 GB",
-        description:"El nuevo iPad se transforma en lo que quieras",
-        price: 85000,
-        img: img3
+        category: "computadoras",
+        title:"MacBook Air M1",
+        description:"El Apple MacBook Air de 13 pulgadas incluye el primer chip de Apple diseñado específicamente para Mac",
+        price: 250000,
+        img: img5
     },
     {
         id: 10,
@@ -93,11 +97,11 @@ const Products = [
     },
     {
         id: 12,
-        category: "tablets",
-        title:"Ipad 10 128 GB",
-        description:"El nuevo iPad se transforma en lo que quieras",
-        price: 85000,
-        img: img3
+        category: "celulares",
+        title:"Pixel 6 128 GB",
+        description:"Pixel 6. Está completamente reinventado, por dentro y por fuera",
+        price: 20000,
+        img: img6
     }
 ]
 
